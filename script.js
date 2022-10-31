4eaecdb29af198e20337dbfdc07cf160efe311fb
jhow <lolrager123@gmail.com>
2022-10-30 17:42:18 -0700

    finished project

---
+++ script.js
@@ -0,0 +1,37 @@
+let btn = document.querySelector('button');
+let div = document.querySelector('.box');
+
+btn.addEventListener('click', getNumber);
+
+
+function getNumber() {
+    let amount = parseInt(prompt('Desired number?'));
+    
+    if (amount == isNaN) { 
+        alert('Please return a number');
+        return;
+    } else if (amount >= 1 && amount <= 100)  {
+        createGrid(amount); 
+    } 
+
+}
+
+
+function createGrid(number) {
+   // resets grid 
+    div.innerHTML="";
+
+    let totalSquares = number * number;
+    
+    for (let i = 0; i < totalSquares; i++) {
+        let createDiv = document.createElement('div');
+        div.appendChild(createDiv);
+        createDiv.addEventListener('mouseover', () => {
+            createDiv.classList.add("color-me");
+        })
+    } 
+    div.style.gridTemplateColumns = `repeat(${number}, 1fr)`;  
+}  
+
+
+
